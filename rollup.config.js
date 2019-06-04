dafc42b2c7bb9a880bb9f32bd33e13a628db9b63
Stephen Baldwin <stephenbaldwin@me.com>
2019-06-04 20:19:39 +0100

    [SB] Spike code for minify of errors

--- rollup.config.js
+++ rollup.config.js
@@ -3,26 +3,11 @@ import commonjs from 'rollup-plugin-commonjs';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript';
 import { terser } from "rollup-plugin-terser";
-<<<<<<< HEAD
-<<<<<<< HEAD
 import copy from 'rollup-plugin-copy';
 const templateLiteralIndentFix = require('./rollup-plugins/template-literal-indent-fix');
-<<<<<<< HEAD
 const minifyErrors = require('./rollup-plugins/error-minify');
 
 const BUILD_DIR = 'build';
-=======
-const templateLiteralIndentFix = require('./rollup-plugins/template-literal-indent-fix');
->>>>>>> [SB] Switch to Rollup
-=======
-const errorMinifier = require('./rollup-plugins/error-minify');
->>>>>>> add build script
-=======
-import copy from 'rollup-plugin-copy';
-const templateLiteralIndentFix = require('./rollup-plugins/template-literal-indent-fix');
-
-const BUILD_DIR = 'build';
->>>>>>> [SB] Switch to Rollup
 
 const WEB_FORMAT = 'iife';
 const NODE_FORMAT = 'cjs';
@@ -30,39 +15,21 @@ const NODE_FORMAT = 'cjs';
 const SOURCEMAP = true;
 const INDENT = false;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> [SB] Switch to Rollup
 const FILES_TO_COPY = [
   './package.json',
   './README.md',
   './src/types.ts',
 ];
 
-<<<<<<< HEAD
-=======
->>>>>>> [SB] Switch to Rollup
-=======
->>>>>>> [SB] Switch to Rollup
 const BASE_PLUGINS = [
   typescript(),
   nodeResolve(),
   commonjs(),
   templateLiteralIndentFix(),
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> [SB] Switch to Rollup
   copy({
     targets: FILES_TO_COPY,
     outputFolder: BUILD_DIR,
   }),
-<<<<<<< HEAD
-=======
->>>>>>> [SB] Switch to Rollup
-=======
->>>>>>> [SB] Switch to Rollup
 ];
 
 const configurations = [];
@@ -94,24 +61,12 @@ function createConfiguration(options) {
 }
 
 function createNodeConfiguration({ type, file, path }) {
-<<<<<<< HEAD
-<<<<<<< HEAD
   const outputFile = `./${BUILD_DIR}/${type}/${file}.js`;
-=======
-  const outputFile = `./dist/${type}/${file}.js`;
->>>>>>> [SB] Switch to Rollup
-=======
-  const outputFile = `./${BUILD_DIR}/${type}/${file}.js`;
->>>>>>> [SB] Switch to Rollup
 
   const configuration = {
     input: path,
     plugins: [
       ...BASE_PLUGINS,
-<<<<<<< HEAD
-      errorMinifier()
-=======
->>>>>>> [SB] Switch to Rollup
     ],
     output: {
       file: outputFile,
@@ -120,30 +75,14 @@ function createNodeConfiguration({ type, file, path }) {
   };
 
   if (!type) {
-<<<<<<< HEAD
-<<<<<<< HEAD
     configuration.output.file = `./${BUILD_DIR}/${file}.js`;
-=======
-    configuration.output.file = `./dist/${file}.js`;
->>>>>>> [SB] Switch to Rollup
-=======
-    configuration.output.file = `./${BUILD_DIR}/${file}.js`;
->>>>>>> [SB] Switch to Rollup
   }
 
   return configuration;
 }
 
 function createDevelopmentConfiguration({ type, file, path, name }) {
-<<<<<<< HEAD
-<<<<<<< HEAD
   const outputFile = `./${BUILD_DIR}/umd/${type}.${file}.development.js`.toLowerCase();
-=======
-  const outputFile = `./umd/${type}.${file}.development.js`.toLowerCase();
->>>>>>> [SB] Switch to Rollup
-=======
-  const outputFile = `./${BUILD_DIR}/umd/${type}.${file}.development.js`.toLowerCase();
->>>>>>> [SB] Switch to Rollup
 
   const configuration = {
     input: path,
@@ -166,15 +105,7 @@ function createDevelopmentConfiguration({ type, file, path, name }) {
   }
 
   if (!type) {
-<<<<<<< HEAD
-<<<<<<< HEAD
     configuration.output.file = `./${BUILD_DIR}/umd/${file}.development.js`;
-=======
-    configuration.output.file = `./umd/${file}.development.js`;
->>>>>>> [SB] Switch to Rollup
-=======
-    configuration.output.file = `./${BUILD_DIR}/umd/${file}.development.js`;
->>>>>>> [SB] Switch to Rollup
   }
 
   return configuration;
@@ -185,24 +116,11 @@ function createProductionConfiguration({ type, file, path, name }) {
     input: path,
     plugins: [
       ...BASE_PLUGINS,
-<<<<<<< HEAD
-<<<<<<< HEAD
       minifyErrors(),
-=======
->>>>>>> [SB] Switch to Rollup
       terser()
     ],
     output: {
       file: `./${BUILD_DIR}/umd/${type}.${file}.production.min.js`.toLowerCase(),
-<<<<<<< HEAD
-=======
-      terser()
-    ],
-    output: {
-      file: `./umd/${type}.${file}.production.min.js`.toLowerCase(),
->>>>>>> [SB] Switch to Rollup
-=======
->>>>>>> [SB] Switch to Rollup
       name: `_ADJS.${type}.${file}`,
 
       format: WEB_FORMAT,
@@ -217,53 +135,12 @@ function createProductionConfiguration({ type, file, path, name }) {
   }
 
   if (!type) {
-<<<<<<< HEAD
-<<<<<<< HEAD
     configuration.output.file = `./${BUILD_DIR}/umd/${file}.production.min.js`;
-=======
-    configuration.output.file = `./umd/${file}.production.min.js`;
->>>>>>> [SB] Switch to Rollup
-=======
-    configuration.output.file = `./${BUILD_DIR}/umd/${file}.production.min.js`;
->>>>>>> [SB] Switch to Rollup
   }
 
   return configuration;
 }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> [SB] Switch to Rollup
-createConfigurations({
-  type: 'Plugins',
-  basePath: './src/plugins',
-  files: [
-    'AutoRender',
-    'AutoRefresh',
-    'Debug',
-    'GenericPlugin',
-    'Logging',
-    'Sticky',
-    'Responsive',
-  ]
-});
-
-createConfigurations({
-  type: 'Networks',
-  basePath: './src/networks',
-  files: [
-    'DFP',
-    'Mock',
-    'Noop',
-  ]
-});
-
-<<<<<<< HEAD
->>>>>>> [SB] Switch to Rollup
-=======
->>>>>>> [SB] Switch to Rollup
 createConfiguration({
   name: 'AdJS',
   path: './src/index.ts',
