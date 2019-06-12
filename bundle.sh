794d915c417d63496b94ed169297e4a6b61deb4f
Matthew Moya <matthewmoya@Matthews-MacBook-Pro.local>
2019-06-12 13:48:53 -0700

    remove rms from script, add documentation

--- bundle.sh
+++ bundle.sh
@@ -1,6 +1,3 @@
-rm ./build/umd/bundle.development.js
-rm ./build/umd/bundle.production.min.js
-
 for f in ./build/umd/* ; do
   if [[ "$f" == *"development"* ]] && [[ "$f" != *"map" ]]; then
     cat $f >> './build/umd/bundle.development.js'
