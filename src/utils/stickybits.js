8d55b9988e5905e14397ee38b6845ddf41fc9b20
crymall <reed.gaines@gmail.com>
2019-06-19 13:59:34 -0400

    change docs, remove plugin

---
+++ src/utils/stickybits.js
@@ -0,0 +1,44 @@
+class Stickybits {
+  constructor (target) {
+    this.props = {
+      stickyBitStickyOffset: 0,
+      verticalPosition: 'top',
+    }
+    this.target = target
+
+    this.props.positionVal = this.definePosition() || 'fixed'
+
+    const {
+      positionVal,
+      verticalPosition,
+      stickyBitStickyOffset,
+    } = this.props
+    const verticalPositionStyle = verticalPosition === 'top' && `${stickyBitStickyOffset}px`
+    const positionStyle = positionVal !== 'fixed' ? positionVal : ''
+
+    // set vertical position
+    this.target.style[verticalPosition] = verticalPositionStyle
+    this.target.style.position = positionStyle
+  }
+
+  definePosition () {
+    let stickyProp;
+    const prefix = ['', '-o-', '-webkit-', '-moz-', '-ms-']
+    const test = document.head.style
+    for (let i = 0; i < prefix.length; i += 1) {
+      test.position = `${prefix[i]}sticky`
+    }
+    stickyProp = test.position ? test.position : 'fixed'
+    test.position = ''
+    return stickyProp;
+  }
+
+  cleanup () {
+      this.target.style.position = ''
+      this.target.style[this.props.verticalPosition] = '';
+  }
+}
+
+export default function stickybits (target) {
+  return new Stickybits(target)
+}
