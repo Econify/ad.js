a7c61fc2cbd806b9ad5c596b7f626a795abf7f43
Matthew Moya <matthewmoya@Matthews-MacBook-Pro.local>
2019-06-24 09:56:03 -0700

    remove bash script first pass

--- rollup.build.js
+++ rollup.build.js
@@ -1,6 +1,10 @@
 const fs = require('fs-extra');
 const rollup = require('rollup');
+const { promisify } = require('util');
 const { configurations, fileSizesObject } = require('./rollup.config.js');
+
+const read = promisify(fs.readFile).bind(fs);
+const write = promisify(fs.writeFile).bind(fs);
 
 const BUILD_DIR = './build';
 
@@ -10,7 +14,7 @@ const FILES_TO_COPY = [
   { path: './src/types.ts', name: 'types.ts' },
 ];
 
-async function createBundle() {
+async function build() {
   return Promise.all(configurations.map(async (config) => {
     const { input, plugins, output } = config;
 
@@ -28,10 +32,31 @@ async function copyFiles() {
   }))
 }
 
+async function createAllInclusiveBundles() {
+  const files = await fs.readdir('./build/umd/');
+  let development = '';
+  let production = '';
+
+  for (let i = 0; i < files.length; i += 1) {
+    const filePath = `./build/umd/${files[i]}`;
+    const dev = filePath.endsWith('development.js');
+    const prod = filePath.endsWith('production.min.js');
+
+    if (dev || prod) {
+      const data = await read(filePath, 'utf8');
+      dev ? development += data : production += data;
+    }
+  }
+
+  await write('./build/umd/bundle.development.js', development);
+  await write('./build/umd/bundle.production.min.js', production);
+}
+
 const startTime = new Date().getTime();
-createBundle()
+build()
   .then(() => fs.writeJson(`${BUILD_DIR}/umd/sizes.json`, fileSizesObject))
   .then(() => copyFiles())
+  .then(() => createAllInclusiveBundles())
   .then(() => {
     const elapsedTime = new Date().getTime() - startTime;
     console.log(`** Build finished in ${elapsedTime}ms **`);
